6d4223e9437496bb04398d3b8963e66dee5c82c1
vbhavanisys <13472bca2024@princescience.in>
2026-09-20 20:25:51 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/ai-resume-ranker/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
